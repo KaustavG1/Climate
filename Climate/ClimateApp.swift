34230965b4972a71f191614512ddb92a3c22f686
Kaustav Ghosh <mail2kg2@gmail.com>
2024-01-26 23:47:44 +0530

    Initital commit

---
+++ Climate/ClimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateApp.swift
+//  Climate
+//
+//  Created by Kaustav Ghosh on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Climate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Climate
+//
+//  Created by Kaustav Ghosh on 26/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
